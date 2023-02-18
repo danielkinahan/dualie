8587e7598c1133cbc99d4584162e74dc963b1ff5
Daniel Kinahan <danielkinahan@uvica.ca>
2023-02-17 19:19:13 -0800

    added midi input and created envelope

--- desktop-synthesizer.cpp
+++ desktop-synthesizer.cpp
@@ -8,63 +8,78 @@ using namespace daisy::seed;
 static DaisySeed hw;
 static Oscillator osc;
 static Encoder enc;
+static MidiUsbHandler midi;
+static AdEnv env;
 
-void AudioCallback(AudioHandle::InterleavingInputBuffer  in, AudioHandle::InterleavingOutputBuffer out, size_t size)
+bool trigger;
+
+void AudioCallback(AudioHandle::InputBuffer  in, AudioHandle::OutputBuffer out, size_t size)
 {
-    float sig;
-    for(size_t i = 0; i < size; i += 2)
-    {
-        sig = osc.Process();
-
-        // left out
-        out[i] = sig;
-
-        // right out
-        out[i + 1] = sig;
+    float env_out;
+    for(size_t i = 0; i < size; i++){
+        
+        env_out = env.Process();
+        osc.SetAmp(env_out);
+    
+        out[0][i] = out[1][i] = osc.Process();
     }
 }
 
 int main(void)
 {
-	float sampleRate;
-	hw.Init();
-	hw.StartLog();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+    hw.Init();
+    hw.StartLog();
+    hw.PrintLine("Hello world!");
 
-    sampleRate = hw.AudioSampleRate();
-    osc.Init(sampleRate);
-	
-	Encoder enc;
-  	enc.Init(hw.GetPin(0), hw.GetPin(2), hw.GetPin(1));
+    trigger = false;
 
-	int tone = 440;
+    // Initialize USB Midi 
+    MidiUsbHandler::Config midi_cfg;
+    midi_cfg.transport_config.periph = MidiUsbTransport::Config::INTERNAL;
+    midi.Init(midi_cfg);
 
-	// Set parameters for oscillator
-    osc.SetWaveform(osc.WAVE_SIN);
-    osc.SetFreq(tone);
-    osc.SetAmp(0);
-	bool noteOn = false;
+    // Initialize test tone
+    osc.Init(hw.AudioSampleRate());
+    env.Init(hw.AudioSampleRate());
 
-	hw.StartAudio(AudioCallback);
+    env.SetTime(ADENV_SEG_ATTACK, 0.15);
+    env.SetTime(ADENV_SEG_DECAY, 0.35);
+    env.SetMin(0.0);
+    env.SetMax(0.25);
+    env.SetCurve(0); // linear
 
-	while(1) {
-		enc.Debounce();
+    osc.SetWaveform(osc.WAVE_TRI);
+    osc.SetFreq(220);
+    osc.SetAmp(0.25);
 
-    	tone += enc.Increment()*100;
-		osc.SetFreq(tone);
-		hw.PrintLine("%i", tone);
+    // start the audio callback
+    hw.StartAudio(AudioCallback);
+    while(1)
+    {
+        // Listen to MIDI
+        midi.Listen();
 
-		if(enc.Pressed()){
-			if(noteOn){
-				noteOn = false;
-				osc.SetAmp(0);
-			}
-			else{
-				noteOn = true;
-				osc.SetAmp(0.5);
-			}
-			hw.DelayMs(200);
-		}
-	}
+        // When message waiting
+        while(midi.HasEvents())
+        {
+            // Take oldest one
+            auto msg = midi.PopEvent();
+            switch(msg.type)
+            {
+                case NoteOn:
+                {
+                    // change freq of oscillator
+                    auto note_msg = msg.AsNoteOn();
+                    if(note_msg.velocity != 0){
+                        osc.SetFreq(mtof(note_msg.note));
+                        env.Trigger();
+                    }
+                }
+                break;
+                    // Since we only care about note-on messages in this example
+                    // we'll ignore all other message types
+                default: break;
+            }
+        }
+    }
 }
