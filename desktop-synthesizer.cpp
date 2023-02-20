ffbc2a7bc85b0b78130a3ed2a582ba321b96cc7a
Daniel Kinahan <danielkinahan@uvica.ca>
2023-02-19 21:13:06 -0800

    added polyphony !

--- desktop-synthesizer.cpp
+++ desktop-synthesizer.cpp
@@ -5,16 +5,8 @@ using namespace daisy;
 using namespace daisysp;
 using namespace daisy::seed;
 
-static DaisySeed      hw;
-static Oscillator     osc, lfo;
-static Encoder        enc;
-static MidiUsbHandler midi;
-static Adsr           env;
-static MoogLadder     flt;
-bool                  gate;
-
 int waveType = 0;
-float ctrlAmp = 0.5;
+float ctrlAmp = 0.75;
 int ctrlAttack = 12;
 int ctrlDecay = 30;
 int ctrlSustain = 25;
@@ -23,140 +15,298 @@ int ctrlFilterFreq = 100;
 int ctrlFilterRes = 10;
 int ctrlLFOFreq = 20;
 
+class Voice
+{
+  public:
+    Voice() {}
+    ~Voice() {}
+    void Init(float samplerate)
+    {
+        active_ = false;
+        osc_.Init(samplerate);
+        osc_.SetAmp(ctrlAmp);
+        osc_.SetWaveform(Oscillator::WAVE_SIN);
+
+        env_.Init(samplerate);
+        env_.SetTime(ADSR_SEG_ATTACK, ctrlAttack / 32.0f);
+        env_.SetTime(ADSR_SEG_DECAY, ctrlDecay / 32.0f);
+        env_.SetSustainLevel(ctrlSustain / 128.0f);
+        env_.SetTime(ADSR_SEG_RELEASE, ctrlRelease / 64.0f);
+    }
+
+    float Process()
+    {
+        if(active_)
+        {
+            float sig, amp;
+            amp = env_.Process(env_gate_);
+            if(!env_.IsRunning())
+                active_ = false;
+            sig = osc_.Process();
+            return sig * (velocity_ / 127.f) * amp;
+        }
+        return 0.f;
+    }
+
+    void OnNoteOn(float note, float velocity)
+    {
+        note_     = note;
+        velocity_ = velocity;
+        osc_.SetFreq(mtof(note_));
+        active_   = true;
+        env_gate_ = true;
+    }
+
+    void OnNoteOff() { env_gate_ = false; }
+
+    void SetParam(int ctrlValue, int param, bool midiCC) 
+    {
+        //Process paramater value changed
+        switch(param)
+        {
+            case 0:
+            {
+                if (midiCC){
+                    waveType = ctrlValue;
+                }
+                else
+                {
+                    waveType += ctrlValue;
+                }
+                
+                switch(waveType / 32)
+                {
+                    case 0:
+                        osc_.SetWaveform(osc_.WAVE_SIN);
+                        break;
+                    case 1:
+                        osc_.SetWaveform(osc_.WAVE_TRI);
+                        break;
+                    case 2:
+                        osc_.SetWaveform(osc_.WAVE_SAW);
+                        break;
+                    case 3:
+                        osc_.SetWaveform(osc_.WAVE_SQUARE);
+                        break;
+                }
+            }
+            case 1:
+            {
+                if (midiCC){
+                    ctrlAttack = ctrlValue;
+                }
+                else
+                {
+                    ctrlAttack += ctrlValue;
+                }
+                env_.SetTime(ADENV_SEG_ATTACK, (ctrlAttack / 32.f));
+            }
+            break;
+            case 2:
+            {
+                if (midiCC){
+                    ctrlDecay = ctrlValue;
+                }
+                else
+                {
+                    ctrlDecay += ctrlValue;
+                }
+                env_.SetTime(ADENV_SEG_DECAY, (ctrlDecay / 32.f));
+            }
+            break;
+            case 3:
+            {
+                if (midiCC){
+                    ctrlSustain = ctrlValue;
+                }
+                else
+                {
+                    ctrlSustain += ctrlValue;
+                }
+                env_.SetSustainLevel(ctrlSustain / 127.f);
+            }
+            break;
+            case 4:
+            {
+                if (midiCC){
+                    ctrlRelease = ctrlValue;
+                }
+                else
+                {
+                    ctrlRelease += ctrlValue;
+                }
+                env_.SetTime(ADSR_SEG_RELEASE, (ctrlRelease / 64.f));
+            }
+            break;
+
+            default: break;
+        }
+    }
+
+    inline bool  IsActive() const { return active_; }
+    inline float GetNote() const { return note_; }
+
+  private:
+    Oscillator osc_;
+    Adsr       env_;
+    float      note_, velocity_;
+    bool       active_;
+    bool       env_gate_;
+};
+
+template <size_t max_voices>
+class VoiceManager
+{
+  public:
+    VoiceManager() {}
+    ~VoiceManager() {}
+
+    void Init(float samplerate)
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            voices[i].Init(samplerate);
+        }
+    }
+
+    float Process()
+    {
+        float sum;
+        sum = 0.f;
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            sum += voices[i].Process();
+        }
+        return sum;
+    }
+
+    void OnNoteOn(float notenumber, float velocity)
+    {
+        Voice *v = FindFreeVoice();
+        if(v == NULL)
+            return;
+        v->OnNoteOn(notenumber, velocity);
+    }
+
+    void OnNoteOff(float notenumber, float velocity)
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            Voice *v = &voices[i];
+            if(v->IsActive() && v->GetNote() == notenumber)
+            {
+                v->OnNoteOff();
+            }
+        }
+    }
+
+    void FreeAllVoices()
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            voices[i].OnNoteOff();
+        }
+    }
+
+    void SetParam(int ctrlValue, int param, bool midiCC)
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            voices[i].SetParam(ctrlValue, param, midiCC);
+        }
+    }
+
+
+  private:
+    Voice  voices[max_voices];
+    Voice *FindFreeVoice()
+    {
+        Voice *v = NULL;
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            if(!voices[i].IsActive())
+            {
+                v = &voices[i];
+                break;
+            }
+        }
+        return v;
+    }
+};
+
+static VoiceManager<24> mgr;
+static DaisySeed        hw;
+//static Oscillator       osc;
+static Oscillator       lfo;
+static Encoder          enc;
+static MidiUsbHandler   midi;
+//static Adsr             env;
+static MoogLadder       flt;
+//bool                    gate;
+
 void AudioCallback(AudioHandle::InputBuffer  in, AudioHandle::OutputBuffer out, size_t size)
 {
-    float env_out, osc_out, freq, modulated_freq;
-    for(size_t i = 0; i < size; i++){
-        env_out = env.Process(gate);
-        osc.SetAmp(env_out);
-        osc_out = osc.Process();
-
-        //flt.SetFreq(ctrlFilterFreq * 174 * lfo.Process());
+    float freq, modulated_freq;
+    for(size_t i = 0; i < size; i++)
+    {
         freq = ctrlFilterFreq * 174;
         modulated_freq = freq + (lfo.Process() * freq);
         flt.SetFreq(modulated_freq);
         
-        out[0][i] = out[1][i] = flt.Process(osc_out);
+        out[0][i] = out[1][i] = flt.Process(mgr.Process() * 0.5f);
     }
 }
 
 void HandleControls(int ctrlValue, int param, bool midiCC)
 {
     //Process paramater value changed
-    switch(param)
+    if (param < 5)
     {
-        case 0:
+        mgr.SetParam(ctrlValue, param, midiCC);
+    }
+    else
+    {
+        switch(param)
         {
-            if (midiCC){
-                waveType = ctrlValue;
-            }
-            else
+            case 5:
             {
-                waveType += ctrlValue;
+                if (midiCC){
+                    ctrlFilterFreq = ctrlValue;
+                }
+                else
+                {
+                    ctrlFilterFreq += ctrlValue;
+                }
+                //flt.SetFreq(ctrlFilterFreq * 174);
             }
-            
-            switch(waveType / 32)
+            break;
+            case 6:
             {
-                case 0:
-                    osc.SetWaveform(osc.WAVE_SIN);
-                    break;
-                case 1:
-                    osc.SetWaveform(osc.WAVE_TRI);
-                    break;
-                case 2:
-                    osc.SetWaveform(osc.WAVE_SAW);
-                    break;
-                case 3:
-                    osc.SetWaveform(osc.WAVE_SQUARE);
-                    break;
+                if (midiCC){
+                    ctrlFilterRes = ctrlValue;
+                }
+                else
+                {
+                    ctrlFilterRes += ctrlValue;
+                }
+                flt.SetRes(ctrlFilterRes / 134.0f);
             }
-        }
-        case 1:
-        {
-            if (midiCC){
-                ctrlAttack = ctrlValue;
-            }
-            else
+            break;
+            case 7:
             {
-                ctrlAttack += ctrlValue;
+                if (midiCC){
+                    ctrlLFOFreq = ctrlValue;
+                }
+                else
+                {
+                    ctrlLFOFreq += ctrlValue;
+                }
+                lfo.SetFreq(ctrlLFOFreq / 6.4f);
             }
-            env.SetTime(ADENV_SEG_ATTACK, (ctrlAttack / 32.f));
-        }
-        break;
-        case 2:
-        {
-            if (midiCC){
-                ctrlDecay = ctrlValue;
-            }
-            else
-            {
-                ctrlDecay += ctrlValue;
-            }
-            env.SetTime(ADENV_SEG_DECAY, (ctrlDecay / 32.f));
-        }
-        break;
-        case 3:
-        {
-            if (midiCC){
-                ctrlSustain = ctrlValue;
-            }
-            else
-            {
-                ctrlSustain += ctrlValue;
-            }
-            env.SetSustainLevel(ctrlSustain / 127.f);
-        }
-        break;
-        case 4:
-        {
-            if (midiCC){
-                ctrlRelease = ctrlValue;
-            }
-            else
-            {
-                ctrlRelease += ctrlValue;
-            }
-            env.SetTime(ADSR_SEG_RELEASE, (ctrlRelease / 64.f));
-        }
-        break;
-        case 5:
-        {
-            if (midiCC){
-                ctrlFilterFreq = ctrlValue;
-            }
-            else
-            {
-                ctrlFilterFreq += ctrlValue;
-            }
-            //flt.SetFreq(ctrlFilterFreq * 174);
-        }
-        break;
-        case 6:
-        {
-            if (midiCC){
-                ctrlFilterRes = ctrlValue;
-            }
-            else
-            {
-                ctrlFilterRes += ctrlValue;
-            }
-            flt.SetRes(ctrlFilterRes / 134.0f);
-        }
-        break;
-        case 7:
-        {
-            if (midiCC){
-                ctrlLFOFreq = ctrlValue;
-            }
-            else
-            {
-                ctrlLFOFreq += ctrlValue;
-            }
-            lfo.SetFreq(ctrlLFOFreq / 6.4f);
-        }
-        break;
+            break;
 
-        default: break;
+            default: break;
+        }
     }
 }
 
@@ -170,20 +320,11 @@ int main(void)
 
     midi.Init(midi_cfg);
     enc.Init(hw.GetPin(0), hw.GetPin(2), hw.GetPin(1));
-    osc.Init(hw.AudioSampleRate());
-    env.Init(hw.AudioSampleRate());
+    mgr.Init(hw.AudioSampleRate());
     flt.Init(hw.AudioSampleRate());
     lfo.Init(hw.AudioSampleRate());
 
     int param = 0;
-
-    env.SetTime(ADENV_SEG_ATTACK, ctrlAttack / 32.0f);
-    env.SetTime(ADENV_SEG_DECAY, ctrlDecay / 32.0f);
-    env.SetSustainLevel(ctrlSustain / 128.0f);
-    env.SetTime(ADSR_SEG_RELEASE, ctrlRelease / 64.0f);
-
-    osc.SetWaveform(osc.WAVE_SIN);
-    osc.SetAmp(ctrlAmp);
 
     //TODO: Change this from linear to logarithmic scaling
     flt.SetFreq(ctrlFilterFreq * 174);
@@ -227,21 +368,21 @@ int main(void)
                 {
                     // change freq of oscillator
                     auto note_msg = msg.AsNoteOn();
-                    if(note_msg.velocity != 0){
-                        osc.SetFreq(mtof(note_msg.note));
-                        gate = true;
-                        env.Retrigger(false);
+                    if(note_msg.velocity != 0)
+                    {
+                        mgr.OnNoteOn(note_msg.note, note_msg.velocity);
                     }
                     else
                     {
-                        gate = false;
+                        mgr.OnNoteOff(note_msg.note, note_msg.velocity);
                     }
                 }
                 break;
 
                 case NoteOff:
                 {
-                    gate = false;
+                    auto note_msg = msg.AsNoteOn();
+                    mgr.OnNoteOff(note_msg.note, note_msg.velocity);
                 }
                 break;
 
@@ -252,7 +393,6 @@ int main(void)
                 }
                 break;
                 
-
                 default: break;
             }
         }
