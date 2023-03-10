5537f4ad2ae141e5920987fda0bb8ecca992455e
Daniel Kinahan <danielkinahan@uvica.ca>
2023-03-09 17:42:53 -0800

    added more efficient moogladder provided by Nick @InfrasonicAudio

--- moogladder.cpp
+++ moogladder.cpp
@@ -1,173 +1,122 @@
+/* Ported from Audio Library for Teensy, Ladder Filter
+ * Copyright (c) 2021, Richard van Hoesel
+ * Copyright (c) 2022, Infrasonic Audio LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
 #include "moogladder.h"
 #include <Utility/dsp.h>
-//#include "arm_math.h"
 
 using namespace custom;
 
-float MoogLadder::my_tanh(float x)
+static inline float fast_tanh(float x)
 {
-    int sign = 1;
-    if(x < 0)
-    {
-        sign = -1;
-        x    = -x;
-        return x * sign;
-    }
-    else if(x >= 4.0f)
-    {
-        return sign;
-    }
-    else if(x < 0.5f)
-    {
-        return x * sign;
-    }
-    return sign * tanhf(x);
+    if (x > 3.0f) return 1.0f;
+    if (x < -3.0f) return -1.0f;
+    float x2 = x * x;
+    return x * (27.0f + x2) / (27.0f + 9.0f * x2);
 }
 
 void MoogLadder::Init(float sample_rate)
 {
     sample_rate_ = sample_rate;
-    istor_       = 0.0f;
-    res_         = 0.4f;
-    freq_        = 1000.0f;
+    alpha_       = 1.0f;
+    K_           = 1.0f;
+    Fbase_       = 1000.0f;
+    Qadjust_     = 1.0f;
+    pbg_         = 0.5f;
+    oldinput_    = 0.f;
 
-    for(int i = 0; i < 6; i++)
+    SetFreq(5000.f);
+    SetRes(0.2f);
+}
+
+float MoogLadder::Process(const float input)
+{
+    float total = 0.0f;
+    float interp = 0.0f;
+    for (size_t os = 0; os < kInterpolation; os++)
     {
-        delay_[i]       = 0.0;
-        tanhstg_[i % 3] = 0.0;
+        float u = (interp * oldinput_ + (1.0f - interp) * input)
+            - (z1_[3] - pbg_ * input) * K_ * Qadjust_;
+        u = fast_tanh(u);
+        float stage1 = LPF(u, 0);
+        float stage2 = LPF(stage1, 1);
+        float stage3 = LPF(stage2, 2);
+        float stage4 = LPF(stage3, 3);
+        total += stage4 * kInterpolationRecip;
+        interp += kInterpolationRecip;
     }
-
-    old_freq_ = 0.0f;
-    old_res_  = -1.0f;
+    oldinput_ = input;
+    return total;
 }
 
 void MoogLadder::ProcessBlock(float *buf, size_t size)
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        float  freq = freq_;
-        float  res  = res_;
-        float  res4;
-        float* delay   = delay_;
-        float* tanhstg = tanhstg_;
-        float  stg[4];
-        float  acr, tune;
-
-        float THERMAL = 0.000025;
-
-        if(res < 0)
-        {
-            res = 0;
+    for (size_t i=0; i < size; i++) {
+        float input = buf[i];
+        float total = 0.0f;
+        float interp = 0.0f;
+        for (size_t os = 0; os < kInterpolation; os++) {
+            float u = (interp * oldinput_ + (1.0f - interp) * input)
+                - (z1_[3] - pbg_ * input) * K_ * Qadjust_;
+            u = fast_tanh(u);
+            float stage1 = LPF(u, 0);
+            float stage2 = LPF(stage1, 1);
+            float stage3 = LPF(stage2, 2);
+            float stage4 = LPF(stage3, 3);
+            total += stage4 * kInterpolationRecip;
+            interp += kInterpolationRecip;
         }
-
-        if(old_freq_ != freq || old_res_ != res)
-        {
-            float f, fc, fc2, fc3, fcr;
-            old_freq_ = freq;
-            fc        = (freq / sample_rate_);
-            f         = 0.5f * fc;
-            fc2       = fc * fc;
-            fc3       = fc2 * fc2;
-
-            fcr  = 1.8730f * fc3 + 0.4955f * fc2 - 0.6490f * fc + 0.9988f;
-            acr  = -3.9364f * fc2 + 1.8409f * fc + 0.9968f;
-            tune = (1.0f - expf(-((2 * PI_F) * f * fcr))) / THERMAL;
-
-            old_res_  = res;
-            old_acr_  = acr;
-            old_tune_ = tune;
-        }
-        else
-        {
-            res  = old_res_;
-            acr  = old_acr_;
-            tune = old_tune_;
-        }
-
-        res4 = 4.0f * res * acr;
-
-        for(int j = 0; j < 2; j++)
-        {
-            buf[i] -= res4 * delay[5];
-            delay[0] = stg[0]
-                = delay[0] + tune * (my_tanh(buf[i] * THERMAL) - tanhstg[0]);
-            for(int k = 1; k < 4; k++)
-            {
-                buf[i]     = stg[k - 1];
-                stg[k] = delay[k]
-                        + tune
-                            * ((tanhstg[k - 1] = my_tanh(buf[i] * THERMAL))
-                                - (k != 3 ? tanhstg[k]
-                                            : my_tanh(delay[k] * THERMAL)));
-                delay[k] = stg[k];
-            }
-            delay[5] = (stg[3] + delay[4]) * 0.5f;
-            delay[4] = stg[3];
-        }
-        buf[i] = delay[5];
+        oldinput_ = input;
+        buf[i] = total;
     }
 }
 
-float MoogLadder::Process(float in)
+void MoogLadder::SetFreq(float freq)
 {
-    float  freq = freq_;
-    float  res  = res_;
-    float  res4;
-    float* delay   = delay_;
-    float* tanhstg = tanhstg_;
-    float  stg[4];
-    float  acr, tune;
+    Fbase_ = freq;
+    compute_coeffs(freq);
+}
 
-    float THERMAL = 0.000025;
+void MoogLadder::SetRes(float res)
+{
+    // maps resonance = 0->1 to K = 0 -> 4
+    res = daisysp::fclamp(res, 0.0f, kMaxResonance);
+    K_ = 4.0f * res;
+}
 
-    if(res < 0)
-    {
-        res = 0;
-    }
+float MoogLadder::LPF(float s, int i)
+{
+    float ft = s * (1.0f/1.3f) + (0.3f/1.3f) * z0_[i] - z1_[i];
+    ft = ft * alpha_ + z1_[i];
+    z1_[i] = ft;
+    z0_[i] = s;
+    return ft;
+}
 
-    if(old_freq_ != freq || old_res_ != res)
-    {
-        float f, fc, fc2, fc3, fcr;
-        old_freq_ = freq;
-        fc        = (freq / sample_rate_);
-        f         = 0.5f * fc;
-        fc2       = fc * fc;
-        fc3       = fc2 * fc2;
-
-        fcr  = 1.8730f * fc3 + 0.4955f * fc2 - 0.6490f * fc + 0.9988f;
-        acr  = -3.9364f * fc2 + 1.8409f * fc + 0.9968f;
-        tune = (1.0f - expf(-((2 * PI_F) * f * fcr))) / THERMAL;
-
-        old_res_  = res;
-        old_acr_  = acr;
-        old_tune_ = tune;
-    }
-    else
-    {
-        res  = old_res_;
-        acr  = old_acr_;
-        tune = old_tune_;
-    }
-
-    res4 = 4.0f * res * acr;
-
-    for(int j = 0; j < 2; j++)
-    {
-        in -= res4 * delay[5];
-        delay[0] = stg[0]
-            = delay[0] + tune * (my_tanh(in * THERMAL) - tanhstg[0]);
-        for(int k = 1; k < 4; k++)
-        {
-            in     = stg[k - 1];
-            stg[k] = delay[k]
-                     + tune
-                           * ((tanhstg[k - 1] = my_tanh(in * THERMAL))
-                              - (k != 3 ? tanhstg[k]
-                                        : my_tanh(delay[k] * THERMAL)));
-            delay[k] = stg[k];
-        }
-        delay[5] = (stg[3] + delay[4]) * 0.5f;
-        delay[4] = stg[3];
-    }
-    return delay[5];
+void MoogLadder::compute_coeffs(float freq)
+{
+    freq = daisysp::fclamp(freq, 5.0f, sample_rate_ * 0.425f);
+    float wc = freq * (float)(2.0f * PI_F / ((float)kInterpolation * sample_rate_));
+    float wc2 = wc * wc;
+    alpha_ = 0.9892f * wc - 0.4324f * wc2 + 0.1381f * wc * wc2 - 0.0202f * wc2 * wc2;
+    Qadjust_ = 1.006f + 0.0536f * wc - 0.095f * wc2 - 0.05f * wc2 * wc2;
 }
